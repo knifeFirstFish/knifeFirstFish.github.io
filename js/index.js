fbc3e5ab9be28ceba40df3eeda6998c1335b3721
knifeFirstFish <1055018195@.qq.com>
2023-09-25 01:09:58 +0800

    提交测试

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const goodsInfo = [
+  { name: "查看>", describe: "质检报告" },
+  { name: "产地", describe: "中国" },
+  { name: "规格", describe: "1.6kg" },
+  { name: "保质期", describe: "365天" },
+  { name: "贮存条件", describe: "冷冻" },
+];
